6f8262b892c9ef7be3cf644364aa2b1fb466476b
Apurvapateriya <apurva.pateriya@gmail.com>
2025-07-29 20:18:22 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -47,4 +47,11 @@ async function sendMessage() {
 
     console.error(error);
   }
+  document.getElementById('send-button').addEventListener('click', sendMessage);
+
+document.getElementById('user-input').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    sendMessage();
+  }
+});
 }
